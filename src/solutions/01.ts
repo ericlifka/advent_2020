8b56948944aa3c0d27ea971917688847adc15b1c
Eric Lifka <eric.lifka@gmail.com>
2020-12-01 11:23:36 -0500

    screw masturbatory recursion, this year is about for loops for speed

--- src/solutions/01.ts
+++ src/solutions/01.ts
@@ -1,26 +1,30 @@
 import { getLines } from '../input-helpers'
 import { parseInteger } from '../converters'
 
-function checkEntry(val, list) {
-  if (!list.length) return false
-
-  let [next, ...rest] = list
-  if (next + val === 2020)
-    return [next, val]
-  else return checkEntry(val, rest)
-}
-
 export const day01part1 = () => {
   let numbers = getLines('01')
     .map(parseInteger)
-  let first
 
-  while (numbers.length > 0) {
-    [first, ...numbers] = numbers
-    let result = checkEntry(first, numbers)
-    if (result)
-      return result[0] * result[1]
+  for (let first = 0; first < numbers.length - 1; first++) {
+    for (let second = first + 1; second < numbers.length; second++) {
+      if (numbers[first] + numbers[second] === 2020) {
+        return numbers[first] * numbers[second]
+      }
+    }
   }
 }
 
-export const day01part2 = () => {}
+export const day01part2 = () => {
+  let numbers = getLines('01')
+    .map(parseInteger)
+
+  for (let first = 0; first < numbers.length - 2; first++) {
+    for (let second = first + 1; second < numbers.length - 1; second++) {
+      for (let third = second + 1; third < numbers.length; third++) {
+        if (numbers[first] + numbers[second] + numbers[third] === 2020) {
+          return numbers[first] * numbers[second] * numbers[third]
+        }
+      }
+    }
+  }
+}
