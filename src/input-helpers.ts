8f6980a48a8b2cd466166c5bb3cecd43c435ca67
Eric Lifka <eric.lifka@gmail.com>
2020-12-01 11:18:53 -0500

    day01part01 done

--- src/input-helpers.ts
+++ src/input-helpers.ts
@@ -1,10 +1,10 @@
-import fs from "fs";
-import { parseInteger } from "./converters";
+import fs from "fs"
+import { parseInteger } from "./converters"
 
 export const getLines = (day) =>
-  fs.readFileSync(`data/${day}`, { encoding: "utf8" }).split("\n").slice(0, -1);
+  fs.readFileSync(`data/${day}.dat`, { encoding: "utf8" }).split("\n").slice(0, -1)
 
-export const getLine = (day) => getLines(day)[0];
+export const getLine = (day) => getLines(day)[0]
 
 export const getNumberArray = (day) =>
-  getLine(day).split(",").map(parseInteger);
+  getLine(day).split(",").map(parseInteger)
--- src/solutions/01.ts
+++ src/solutions/01.ts
@@ -1,3 +1,26 @@
-export const day01part1 = () => {}
+import { getLines } from '../input-helpers'
+import { parseInteger } from '../converters'
+
+function checkEntry(val, list) {
+  if (!list.length) return false
+
+  let [next, ...rest] = list
+  if (next + val === 2020)
+    return [next, val]
+  else return checkEntry(val, rest)
+}
+
+export const day01part1 = () => {
+  let numbers = getLines('01')
+    .map(parseInteger)
+  let first
+
+  while (numbers.length > 0) {
+    [first, ...numbers] = numbers
+    let result = checkEntry(first, numbers)
+    if (result)
+      return result[0] * result[1]
+  }
+}
 
 export const day01part2 = () => {}
