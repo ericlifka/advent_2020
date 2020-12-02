66ab8bb76a144f1d85e501179b975885197652b9
Eric Lifka <eric.lifka@gmail.com>
2020-12-02 01:47:29 -0500

    move dat reading to outside functions so it's not part of the timing

--- src/solutions/01.ts
+++ src/solutions/01.ts
@@ -1,10 +1,10 @@
 import { getLines } from '../input-helpers'
 import { parseInteger } from '../converters'
 
-export const day01part1 = () => {
-  let numbers = getLines('01')
-    .map(parseInteger)
+let numbers = getLines('01')
+  .map(parseInteger)
 
+export const day01part1 = () => {
   for (let first = 0; first < numbers.length - 1; first++) {
     for (let second = first + 1; second < numbers.length; second++) {
       if (numbers[first] + numbers[second] === 2020) {
@@ -15,9 +15,6 @@ export const day01part1 = () => {
 }
 
 export const day01part2 = () => {
-  let numbers = getLines('01')
-    .map(parseInteger)
-
   for (let first = 0; first < numbers.length - 2; first++) {
     for (let second = first + 1; second < numbers.length - 1; second++) {
       for (let third = second + 1; third < numbers.length; third++) {
