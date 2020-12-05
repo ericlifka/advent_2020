aafb6e65018edf29091c0e02d28e748cb4d36b49
Eric Lifka <eric.lifka@gmail.com>
2020-12-05 03:29:27 -0500

    silly clean up

--- src/solutions/05.ts
+++ src/solutions/05.ts
@@ -1,20 +1,17 @@
 import { getLines } from '../input-helpers'
 
+type Range = [ number, number ]
+
 let passes = getLines('05')
   .map(line => ({
     col: line.slice(7),
     row: line.slice(0, 7)
   }))
 
-type Range = [ number, number ]
-
-const reduceRange = (letter, [lower, upper]): Range => {
-  if (letter === 'F' || letter === 'L') {
-    return [ lower, (upper - lower - 1) / 2 + lower ]
-  } else {
-    return [ (upper - lower + 1) / 2 + lower, upper ]
-  }
-}
+const reduceRange = (letter, [lower, upper]): Range =>
+  (letter === 'F' || letter === 'L')
+    ? [ lower, (upper - lower - 1) / 2 + lower ]
+    : [ (upper - lower + 1) / 2 + lower, upper ]
 
 const runCode = (code): number => {
   let range: Range = code.length === 7 ? [0, 127] : [0, 7]
@@ -27,23 +24,18 @@ const runCode = (code): number => {
 const calcSeatId = ({col, row}) => runCode(row) * 8 + runCode(col)
 
 export const day05part1 = () => {
-  let max = 0
-  passes.forEach(pass => {
-    let id = calcSeatId(pass)
-    if (id > max)
-      max = id
-  })
-  return max
+  return passes
+    .map(pass => calcSeatId(pass))
+    .sort((l, r) => r - l)
+    [0]
 }
 
 export const day05part2 = () => {
-  let seats = []
-  passes.forEach(pass => seats.push(calcSeatId(pass)))
-  seats = seats.sort((l, r) => l - r)
+  let seats = passes
+    .map(pass => calcSeatId(pass))
+    .sort((l, r) => l - r)
 
-  for (let i = 0; i < seats.length - 1; i++)
-    if (seats[i] + 1 !== seats[i + 1])
+  for (let i of seats)
+    if (seats[i + 1] - seats[i] > 1)
       return seats[i] + 1
-
-  return seats
 }
