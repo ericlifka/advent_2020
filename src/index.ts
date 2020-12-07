86eaf91aa0d3b6a1b08c0065663ee91f4c276bdf
Eric Lifka <eric.lifka@gmail.com>
2020-12-07 13:54:30 -0500

    day09 both parts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { day05part1, day05part2 } from "./solutions/05"
 import { day06part1, day06part2 } from "./solutions/06"
 import { day07part1, day07part2 } from "./solutions/07"
 import { day08part1, day08part2 } from "./solutions/08"
+import { day09part1, day09part2 } from "./solutions/09"
 
 const solutions =
 { '011': day01part1, '012': day01part2
@@ -17,6 +18,7 @@ const solutions =
 , '061': day06part1, '062': day06part2
 , '071': day07part1, '072': day07part2
 , '081': day08part1, '082': day08part2
+, '091': day09part1, '092': day09part2
 }
 
 let day = process.argv[ 2 ]
---
+++ src/solutions/09.ts
@@ -0,0 +1,54 @@
+import { parseInteger } from '../converters'
+import { getLines } from '../input-helpers'
+
+const numbers = getLines('09').map(parseInteger)
+
+const checkNumber = (position) => {
+  let target = numbers[position]
+  let start = position - 25
+  let end = position
+
+  for (let i = start; i < end - 1; i++)
+    for (let k = i + 1; k < end; k++)
+      if (numbers[i] + numbers[k] === target)
+        return true
+
+  return false
+}
+
+export const day09part1 = () => {
+  let position = 25
+  while (checkNumber(position))
+    position++
+  
+  return numbers[position]
+}
+
+export const day09part2 = () => {
+  let target = 675280050
+  let start = 0
+  
+  while (true) {
+    let position = start
+    let sum = 0
+    let min = Infinity
+    let max = 0
+    while (sum < target) {
+      let current = numbers[position]
+      sum += current
+
+      if (current < min)
+        min = current
+
+      if (current > max)
+        max = current
+
+      position++
+    }
+
+    if (sum === target)
+      return min + max
+    else
+      start++
+  }
+}
