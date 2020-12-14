9e9fac9ec596cebac64e989ed48fedd48c41eb17
Eric Lifka <eric.lifka@gmail.com>
2020-12-13 23:51:35 -0500

    day 14 prep

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import { day11part1 } from "./solutions/11_1"
 import { day11part2 } from "./solutions/11_2"
 import { day12part1, day12part2 } from "./solutions/12"
 import { day13part1, day13part2 } from "./solutions/13"
+import { day14part1, day14part2 } from "./solutions/14"
 
 const solutions =
 { '011': day01part1, '012': day01part2
@@ -28,6 +29,7 @@ const solutions =
 , '111': day11part1, '112': day11part2
 , '121': day12part1, '122': day12part2
 , '131': day13part1, '132': day13part2
+, '141': day14part1, '142': day14part2
 }
 
 let day = process.argv[ 2 ]
---
+++ src/solutions/14.ts
@@ -0,0 +1,11 @@
+import { getLines } from '../input-helpers'
+
+let lines = getLines('14')
+
+export const day14part1 = () => {
+
+}
+
+export const day14part2 = () => {
+
+}
