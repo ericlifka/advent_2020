6c5833073a5b656ce0ad7e4068aae88a55b7808e
Eric Lifka <eric.lifka@gmail.com>
2020-12-15 22:18:24 -0500

    improve the memory pressure for day 15 so it runs much faster

--- src/solutions/15.ts
+++ src/solutions/15.ts
@@ -26,7 +26,7 @@ export const day15part1 = () => {
     } else {
       nums[next].unshift(turn)
     }
-    // console.log(`${turn}: ${next}`)
+
     last = next
     turn++
     
@@ -35,36 +35,18 @@ export const day15part1 = () => {
 }
 
 export const day15part2 = () => {
-  let nums = {
-    1: [1],
-    12: [2],
-    0: [3],
-    20: [4],
-    8: [5],
-    16: [6]
-  }
-  let turn = 7
-  let last = 16
-  while (turn <= 30000000) {
-    let next
-    if (nums[last].length == 1) {
-      next = 0
-    } else {
-      next = nums[last][0] - nums[last][1]
-    }
+  let nums = new Array(30000000)
+  seed.forEach((n, i) => nums[n] = i+1)
+  let last = seed[seed.length - 1]
+  let turn = seed.length
+  while (turn < 30000000) {
+    let next = (last in nums)
+      ? turn - nums[last]
+      : 0
 
-    if (!nums[next]) {
-      nums[next] = [turn]
-    } else {
-      nums[next][1] = nums[next][0]
-      nums[next][0] = turn
-    }
-    // console.log(`${turn}: ${next}`)
+    nums[last] = turn
     last = next
     turn++
-    if (turn % 1000000 == 0) {
-      console.log(turn)
-    }
   }
   return last
 }
