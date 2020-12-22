179873f75178b021cda953b510b1bebe96d3c05c
Eric Lifka <eric.lifka@gmail.com>
2020-12-21 21:23:07 -0500

    day 19. part 1 works, 2 doesnt

--- src/solutions/19.ts
+++ src/solutions/19.ts
@@ -4,40 +4,94 @@ import { isLineBreak } from 'typescript'
 
 
 let rules = {}
+let messages;
 
-getLines('19_rules')
-  .map(line => line.split(': '))
-  .map(([rule, text]) => 
-    (/"."/.test(text))
-      ? { rule, char: text[1] }
-      : {
-        rule,
-        segments: text.split(' | ').map(segment => segment.split(' '))
-      }
-  )
-  .forEach(r => {
-    rules[r.rule] = r
-  })
+const getInput = () => {
+  messages = getLines('19_messages')
 
-let messages = getLines('19_messages')
+  getLines('19_rules')
+    .map(line => line.split(': '))
+    .map(([rule, text]) => 
+      (/"."/.test(text))
+        ? { rule, char: text[1] }
+        : {
+          rule,
+          segments: text.split(' | ').map(segment => segment.split(' '))
+        }
+    )
+    .forEach(r => {
+      rules[r.rule] = r
+    })
 
-// console.log(rules)
-// rules.forEach(r => console.log(r))
+  // console.log(messages)
+  // for (let rule in rules) {
+  //   console.log(rules[ rule ])
+  // }
+}
 
+function testRule(string, position, rule) {
+  let ruleDef = rules[ rule ]
+  
+  if (ruleDef.char) {
+    if (string[ position ] == ruleDef.char) {
+      return position + 1
+    } else {
+      throw `mismatch at postion ${position}, expected ${ruleDef.char} but got ${string[ position ]}`
+    }
+  }
 
+  let match = false
+  for (let segment of ruleDef.segments) {
+    try {
+      position = testSegment(string, position, segment)
+      match = true
+    } catch (e) { }
+    
+    if (match)
+      break
+  }
 
-export const day19part1 = () => {
-  let message = messages[0]
-  let rule = rules['0']
-  let position = 0
-  let stack = [ ...rule.segments ]
-  console.log(stack)
-
-  while (stack.length > 0) {
-    let next = stack.shift()
+  if (match) {
+    return position
+  } else {
+    throw `mismatch at position ${position}, couldn't match any segments: ${ruleDef.segments.map(s => s.join(' ')).join(' | ')}`
   }
 }
 
+function testSegment(string, position, segment) {
+  segment.forEach(rule => {
+    position = testRule(string, position, rule)
+  })
+  return position
+}
+
+function testString(string) {
+  try {
+    let position = testRule(string, 0, '0')
+    return position == string.length
+  } catch (e) {
+    return false
+  }
+}
+
+function countMatches() {
+  let count = 0
+  messages.forEach(message => {
+    if (testString(message))
+      count++
+  })
+  return count
+}
+
+export const day19part1 = () => {
+  getInput()
+
+  return countMatches()
+}
+
 export const day19part2 = () => {
-  
+  rules['8'] = { rule: '8', segments: [ ['42'], ['42', '8'] ] }
+  rules['11'] = { rule: '11', segments: [ ['42', '31'], ['42', '11', '31'] ] }
+
+  return countMatches()
 }
